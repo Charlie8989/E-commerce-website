29f10cf35ef36eafb7d7322705b620cb36142c40
Charlie <charlie962873@gmail.com>
2026-02-16 21:38:43 +0530

    some changes

--- backend/routes/emailRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendDiscountEmail } from "../controllers/emailController.js";
-
-const emailRouter = express.Router();
-
-emailRouter.post("/discount", sendDiscountEmail);
-
-export default emailRouter;

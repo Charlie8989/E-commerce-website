e3643d1979948a19032d58c8e284bbb26bd8bb29
Ayush Sahu <134526994+Charlie8989@users.noreply.github.com>
2026-02-16 19:39:43 +0530

    vite config popup allow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { port: 5174 },
+  server: {
+    port: 5174,
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
+  },
 });
